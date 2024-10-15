8750cf0c5784382bc45a6dd6bc9c2e2c891ccbc6
Patricia Duarte de Lima Machado <40029758+pdlmachado@users.noreply.github.com>
2024-10-15 17:39:33 -0300

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,19 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
+    formatted_response = json.loads(response.text)
+    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    response_dict = {"anger": anger, "disgust": disgust, "fear": fear, "joy": joy, 
+                     "sadness": sadness}
+    dominant_emotion_key = max(zip(response_dict.values(), response_dict.keys()))[1]   
+    response_dict["dominant_emotion"] = dominant_emotion_key             
+    return response_dict
